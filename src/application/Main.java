c17a40eceb7715b37e418be532609ef4ea9f4a6e
Rafael-Milhim <rafael.milhim@hotmail.com>
2023-08-17 08:58:03 -0300

    Printing possible moves

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,8 +8,8 @@ import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
-public class Programing {
-	public static void main(String[] args) {
+public class Main {
+public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
@@ -21,6 +21,10 @@ public class Programing {
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
+				
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 	
 				System.out.println();
 				System.out.print("Target: ");
